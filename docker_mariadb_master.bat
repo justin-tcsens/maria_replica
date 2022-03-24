ee682ca83ec5e1cdca254c7dd043b4f123aea2c7
Justin Phua <justin.phua@tcsens.com>
2022-03-24 15:45:10 +0800

    Update script

--- docker_mariadb_master.bat
+++ docker_mariadb_master.bat
@@ -1,7 +1,5 @@
+docker network create jpj_training_network
 
 docker rm mariadb_master
-
-@REM docker run --name mariadb_master --network jpj_training_network --network-alias maria_master -v /D/Training_Workspace/maria_replica/master/data:/var/lib/mysql -v /D/Training_Workspace/maria_replica/master/log:/var/log/mysql -e MYSQL_USER=replica_user -e MYSQL_ROOT_PASSWORD=root -e MYSQL_PASSWORD=password -p 3350:3306 -d mariadb:10.6.5 --log-bin --binlog-format=MIXED --log-basename=master --server-id=1
-
 
 docker run --name mariadb_master --network jpj_training_network --network-alias maria_master -v /D/Training_Workspace/maria_replica/master/data:/var/lib/mysql -v /D/Training_Workspace/maria_replica/master/log:/var/log/mysql -e MYSQL_USER=app -e MYSQL_DATABASE=jpj-training -e MYSQL_ROOT_PASSWORD=root -e MYSQL_PASSWORD=password -p 3306:3306 -d mariadb:10.6.5 --log-bin --binlog-format=MIXED --log-basename=master --server-id=1
--- docker_mariadb_slave.bat
+++ docker_mariadb_slave.bat
@@ -1,9 +1,5 @@
+docker network create jpj_training_network
+
 docker rm mariadb_slave
-
-@REM docker run --name mariadb_slave --network jpj_training_network --network-alias maria_slave -v /D/Training_Workspace/maria_replica/slave/data:/var/lib/mysql -v /D/Training_Workspace/maria_replica/slave/log:/var/log/mysql -e MYSQL_USER=replica_user -e MYSQL_ROOT_PASSWORD=root -e MYSQL_PASSWORD=password -p 3351:3306 -d mariadb:10.6.5 --log-bin=/var/lib/mysql/mysql-bin --log_bin_index=/var/lib/mysql/mysql-bin --binlog-format=MIXED --server-id=2 --relay_log=/var/lib/mysql/relay-bin --relay_log_index=/var/lib/mysql/relay-bin.index --relay_log_info_file=/var/lib/mysql/relay-bin.info
-
-
-
-@REM docker run --name mariadb_slave --network jpj_training_network --network-alias maria_slave -v /D/Training_Workspace/maria_replica/slave/data:/var/lib/mysql -v /D/Training_Workspace/maria_replica/slave/log:/var/log/mysql -e MYSQL_USER=app -e MYSQL_ROOT_PASSWORD=root -e MYSQL_PASSWORD=password -p 3351:3306 -d mariadb:10.6.5 --log-bin=/var/lib/mysql/mysql-bin --log_bin_index=/var/lib/mysql/mysql-bin --binlog-format=MIXED --server-id=2 --relay_log=/var/lib/mysql/relay-bin --relay_log_index=/var/lib/mysql/relay-bin.index --relay_log_info_file=/var/lib/mysql/relay-bin.info
 
 docker run --name mariadb_slave --network jpj_training_network --network-alias maria_slave -v /D/Training_Workspace/maria_replica/slave/data:/var/lib/mysql -v /D/Training_Workspace/maria_replica/slave/log:/var/log/mysql -e MYSQL_USER=app -e MYSQL_ROOT_PASSWORD=root -e MYSQL_PASSWORD=password -p 3351:3306 -d mariadb:10.6.5 --server-id=2 
